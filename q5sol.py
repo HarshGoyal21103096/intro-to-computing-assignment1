c36904220ac83d642b90cd14ed4b0e6e8991ff95
HarshGoyal21103096 <97400616+HarshGoyal21103096@users.noreply.github.com>
2022-01-12 09:54:18 +0530

    Add files via upload

---
+++ q5sol.py
@@ -0,0 +1,10 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===================== RESTART: F:/python assignments/q5.py =====================
+(a)
+['red', 'green', 'white', 'black', 'pink', 'yellow']
+['red', 'green', 'white', 'pink', 'yellow']
+(b)
+['red', 'green', 'white', 'black', 'pink', 'yellow']
+['red', 'green', 'white', 'purple', 'purple', 'yellow']
