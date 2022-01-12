e7d14a4e679ad5ac796c476a139c0523cbee35e8
HarshGoyal21103096 <97400616+HarshGoyal21103096@users.noreply.github.com>
2022-01-12 09:45:18 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,10 @@
+#ques1:take average of 3 numbers taken from the user
+number1=input("give number 1=")
+number2=input("give number 2=")
+number3=input("give number 3=")
+number1=int(number1)
+number2=int(number2)
+number3=int(number3)
+#average
+average=(number1+number2+number3)/3
+print(average)
