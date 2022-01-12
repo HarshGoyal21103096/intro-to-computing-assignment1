6997d8327c24b7255aee3afb3d869800d630bab2
HarshGoyal21103096 <97400616+HarshGoyal21103096@users.noreply.github.com>
2022-01-12 09:48:58 +0530

    Add files via upload

---
+++ q1sol.py
@@ -0,0 +1,8 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===================== RESTART: F:\python assignments\q1.py =====================
+give number 1=47
+give number 2=56
+give number 3=71
+58.0
