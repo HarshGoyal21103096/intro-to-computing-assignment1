7bcdc6c160a478d7eb218305d0d9c7d6205e3482
HarshGoyal21103096 <97400616+HarshGoyal21103096@users.noreply.github.com>
2022-01-12 09:52:40 +0530

    Add files via upload

---
+++ q3sol.py
@@ -0,0 +1,11 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========================================================================= RESTART: F:/python assignments/q3.py =========================================================================
+enter name=Harsh Goyal
+enter student Id=21103096
+enter gender=male
+enter course name=computer science
+enter cgpa=10.0
+['name', 'SID', 'gender', 'course name', 'cgpa']
+['Harsh Goyal', 21103096, 'male', 'computer science', 10.0]
